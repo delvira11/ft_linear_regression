a236c8a0447a9f485ddbb05503cb8b72eec72e1b
David <davidelvira.val@gmail.com>
2023-12-29 03:17:16 +0100

    fixed things

--- ft_linear_regression.py
+++ ft_linear_regression.py
@@ -1,8 +1,7 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
-
+from sklearn.linear_model import LinearRegression
 
 
 
@@ -13,7 +12,7 @@ def loss_function(data, theta0, theta1):
     for i in range(len(data)):
         total_error += abs(data.iloc[i]["price"] - (theta0 + (theta1 * data.iloc[i]["km"])))
 
-    print(total_error)
+    #print(total_error)
     return total_error
 
 
@@ -33,6 +32,29 @@ def gradient_descent(data, theta0, theta1, L):
 
 
 
+def predict(theta0, theta1, km):
+    price = theta0 + (theta1 * km)
+    return price
+
+def plot_line(data, theta0, theta1):
+    plt.scatter(data["km"], data["price"])
+    
+    print ("theta0 = ", theta0, "theta1 = ", theta1)
+    print(predict(theta0, theta1, data["km"].min()))
+    
+    plt.plot([data["km"].min(), data["km"].max()], [predict(theta0, theta1, data["km"].min()), predict(theta0, theta1, data["km"].max())], c="red")
+    
+    lin_reg = LinearRegression()
+    lin_reg.fit(data[["km"]], data[["price"]])
+    
+    pricemin = lin_reg.predict([[data["km"].min()]])
+    pricemax = lin_reg.predict([[data["km"].max()]])
+    
+    plt.plot([data["km"].min(), data["km"].max()], [pricemin.reshape(-1, 1)[0][0], pricemax.reshape(-1, 1)[0][0]], 'r--', c="green")
+
+    
+    
+    plt.show()
 
 def main():
     theta0  = 0 # Constant term
@@ -40,12 +62,13 @@ def main():
     epochs = 1000
     L = 0.01 # Learning rate
     data = pd.read_csv("data.csv")
+
     scaler = StandardScaler()
     data["km"] = scaler.fit_transform(data[["km"]])
     for i in range(epochs):
         theta0, theta1 = gradient_descent(data, theta0, theta1, L)
         loss_function(data, theta0, theta1)
+    plot_line(data, theta0, theta1)
 
-    
 if __name__ == "__main__":
     main()
